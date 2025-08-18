d4c2f2e0816700b62f54021f18af2019f728006a
Edwin Marcial <edwin_ma@yahoo.com>
2025-08-17 20:07:00 -0400

    update

--- standings.js
+++ standings.js
@@ -5,14 +5,14 @@ var debug = require ('./debug');
 */
 var standings2025 = [
         
-    new Standing ("Cigars", "10", "3", "0"),
-    new Standing ("Black Sox", "6", "6", "0"),
-    new Standing ("Midtown Magic", "0", "14", "0"),
-    new Standing ("Dragons", "11", "3", "0"),
-    new Standing ("Cherokees", "7", "6", "0"),
-    new Standing ("Punishers", "4", "9", "0"),
+    new Standing ("Cigars", "12", "3", "0"),
+    new Standing ("Black Sox", "7", "6", "0"),
+    new Standing ("Midtown Magic", "0", "15", "0"),
+    new Standing ("Dragons", "11", "4", "0"),
+    new Standing ("Cherokees", "8", "6", "0"),
+    new Standing ("Punishers", "4", "10", "0"),
     new Standing ("Squeaks", "5", "8", "0"),
-    new Standing ("Cobb Angels", "4", "9", "1")
+    new Standing ("Cobb Angels", "5", "10", "1")
     ];
 
 
