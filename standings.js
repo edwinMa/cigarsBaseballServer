b5b9d2252fa4e17cf8fc0f16f5cd68c8be83619f
Edwin Marcial <edwin_ma@yahoo.com>
2026-03-24 15:53:30 -0400

    restore original code

---
+++ standings.js
@@ -0,0 +1,59 @@
+var debug = require ('./debug');
+
+/*
+** 2025 standings
+*/
+var standings2025 = [
+        
+    new Standing ("Cigars", "15", "3", "0"),
+    new Standing ("Black Sox", "7", "11", "0"),
+    new Standing ("Midtown Magic", "0", "16", "1"),
+    new Standing ("Dragons", "13", "5", "0"),
+    new Standing ("Cherokees", "10", "7", "0"),
+    new Standing ("Punishers", "7", "11", "0"),
+    new Standing ("Squeaks", "6", "11", "0"),
+    new Standing ("Cobb Angels", "7", "11", "")
+    ];
+
+
+/*
+ ** function to create a schedule item
+ */
+function Standing(team, wins, losses, ties)
+{
+    this.team = team;
+    this.wins = wins;
+    this.losses = losses;
+    this.ties = ties;
+}
+
+
+function Standings()
+{
+    // last column is for evite link
+    this.events = standings2025;
+    this.year = "2025";
+}
+
+
+Standings.prototype = {
+    // set constructors 
+    constructor: Standings,
+
+    // set methods
+    getStandings: function()
+    {
+        var result = this.events;
+        debug(result);
+        return (result);
+    }
+
+};
+
+
+module.exports = new Standings();
+
+
+
+            
+            
